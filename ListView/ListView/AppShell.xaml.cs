36768dc50e604b7d8171776c86517bfad91a9f72
AquaDarknessMegumin <shawnryannacario@gmail.com>
2026-03-25 13:41:11 +0800

    Commit

--- ListView/ListView/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ListView;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
---
+++ Midterms/Midterms/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Midterms;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
